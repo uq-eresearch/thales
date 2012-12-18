3c4dd532987db0cc1ffe2f9f7169a0eca1dc6ead
Hoylen Sue <h.sue@uq.edu.au>
2012-12-18 12:05:57 +1000

    Adding RSpec routing tests.

---
+++ spec/routing/settings_routing_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe SettingsController do
+  describe "routing" do
+
+    it "routes to #index" do
+      get("/settings").should route_to("settings#index")
+    end
+
+    it "routes to #edit" do
+      get("/settings/1/edit").should route_to("settings#edit", :id => "1")
+    end
+
+    it "routes to #update" do
+      put("/settings/1").should route_to("settings#update", :id => "1")
+    end
+
+  end
+end
