232d6cc8526d8167ffecdd2895365ce19e42da7d
Hoylen Sue <h.sue@uq.edu.au>
2012-12-21 13:01:20 +1000

    Adding explicit setup of delete support level.

--- lib/thales/output/oaipmh/record_provider.rb
+++ lib/thales/output/oaipmh/record_provider.rb
@@ -28,12 +28,23 @@ module Thales
 
         source_model OAI::Provider::ActiveRecordWrapper.new(OaipmhRecord)
 
+        # Delete support: can be set to PERSISENT if the deployment
+        # and management policies ensure it.
+        deletion_support OAI::Const::Delete::TRANSIENT
+
         register_format(RifcsFormat.instance)
 
         # Returns the prefix used by ruby-oai to create the OAI-PMH
         # unique identifers. Each record in the OAI-PMH feed has
         # a unique identifier that is this prefix concatenated with
         # a slash character and the OAI-PMH record.id.
+        # 
+        # Normally the prefix is just a member variable the is set
+        # using the record_prefix (which is an alias for prefix=).
+        # But we make it a method here so we can dynamically
+        # at run time use the hostname of the requested URL.
+        # Actually, this is a bit dodgy because if the request
+        # URL is different, they will get a different identifier.
         #
         # See the identifier_for method in ruby-oai's
         # oai/provider/response/record_response.rb file.
